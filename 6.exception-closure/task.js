abfac42a83518ca698e5084a5adec688a19a2c0f
Zinnatullin Danil <d_zinnatullin@inbox.ru>
2025-06-13 19:19:34 +0500

    Выполнил зание на замыкание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,69 +1,59 @@
 ﻿// Задача 1
 function parseCount(value) {
-  const parsed = Number.parseFloat(value);
-  if (Number.isNaN(parsed)) {
-    throw new Error('Невалидное значение');
+  const number = Number.parseFloat(value)
+  if(Number.isNaN(number)) {
+    throw new Error('Невалидное значение')
   }
-  return parsed;
+  return number
 }
 
 function validateCount(value) {
   try {
-    return parseCount(value);
+      return parseCount(value)
   } catch (error) {
-    return error;
+    return error
   }
 }
 
-module.exports = { parseCount, validateCount };
-
 // Задача 2
-function canExist(a, b, c) {
-  return a + b > c && a + c > b && b + c > a;
+function isTriangele(a, b, c) {
+  if(!(a + b > c && a + c > b && b + c > a)) {
+    throw new Error('Треугольник с такими сторонами не существует')
+  }
 }
-
 class Triangle {
   constructor(a, b, c) {
-    if (!canExist(a, b, c)) {
-      throw new Error('Треугольник с такими сторонами не существует');
-    }
+    this.a = a
+    this.b = b
+    this.c = c
 
-    this.a = a;
-    this.b = b;
-    this.c = c;
-
-    Object.freeze(this);
+    isTriangele(a, b, c)
   }
 
   get perimeter() {
-    return this.a + this.b + this.c;
+    return this.a + this.b + this.c
   }
 
   get area() {
-    const p = this.perimeter / 2;                       // полупериметр
-    const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-    return Number(s.toFixed(3));
+    const p = this.perimeter / 2
+    const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))
+    return Number(s.toFixed(3))
   }
 }
 
 function getTriangle(a, b, c) {
   try {
-    return new Triangle(a, b, c);
+    return new Triangle(a, b, c)
   } catch {
-    const errorMsg = 'Ошибка! Треугольник не существует';
-
-    const stub = {
+    const erroMsg = 'Ошибка! Треугольник не существует'   
+    const ErrorObj = {
       get area() {
-        return errorMsg;
+        return erroMsg
       },
       get perimeter() {
-        return errorMsg;
+        return erroMsg
       }
-    };
-
-    Object.freeze(stub);
-    return stub;
+    }
+    return ErrorObj
   }
 }
-
-module.exports = { Triangle, getTriangle };
